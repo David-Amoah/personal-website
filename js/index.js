a209aee5373559fd7d898f9657576ce0215400f1
David Amoah <amoahcorey@gmail.com>
2021-02-19 09:26:02 +0000

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 const texts = document.querySelectorAll(".inner")
-const faces = document.querySelectorAll(".faces")
+const faces = document.querySelectorAll(".face")
 
-console.log(texts)
+faces.forEach(function(face, index) {
+    face.addEventListener("click", function() {
+        clear()
+
+        this.classList.add("active")
+
+        texts[index].classList.add("active")
+        console.log(texts[index])
+    })
+})
+
+function clear() {
+    texts.forEach((text) => text.classList.remove("active"));
+    faces.forEach((face) => face.classList.remove("active"));
+    console.log("cleared")
+}
